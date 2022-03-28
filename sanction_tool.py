6bcee46b4b9061a062167d36bc0996e6e27e41df
shailesharma21 <shailesh.sharma@tigeranalytics.com>
2022-03-28 18:54:51 +0530

    upload function changes

--- sanction_tool.py
+++ sanction_tool.py
@@ -2,29 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from matching_utils import f_name_match_score
-from utils import scrape_uk_sanctions
-from config import sanction_file_name
+from utils import uk_sanction_list
 import itertools
-from pandas_ods_reader import read_ods
-
 
 st.header("Inchcape Supplier Sanctions Checker")
 
-
-# load uk sanction list
-@st.cache
-def uk_sanction_data_load(sanctions_file):
-    scrape_uk_sanctions()
-    df2 = read_ods(sanctions_file)
-    df2 = df2.iloc[1:]
-    df2.columns = df2.iloc[0]
-    df2 = df2.drop(df2.index[0])
-    #uk_sanctions = df2.loc[df2['Unique ID'].str.startswith('RUS')]
-    uk_sanctions = df2.loc[df2['Individual, Entity, Ship']=='Entity']
-    return uk_sanctions
-
-# load uk sanctioned data
-uk_sanction_list = uk_sanction_data_load(sanction_file_name)
 
 # Entity matching based on sanction list and entered name by user
 @st.cache
@@ -33,6 +15,7 @@ def entity_matching(search_name, uk_sanction_list):
     for j,cc in zip(uk_sanction_list['Name 6'],uk_sanction_list['Unique ID']):
         if j not in t_dict.keys():
             t_dict[j,cc] = int(f_name_match_score(search_name, j))
+    
     lst = list(t_dict.items())
     df = pd.DataFrame(lst,columns=["sanction name","Score"])
     df[["Match","Country Code"]] = pd.DataFrame(
@@ -81,7 +64,7 @@ if button_clicked:
             unsafe_allow_html=True,
         )
     most_probable_match = matched.loc[
-        (matched["Score"] > 60) & (matched["Score"] < 95)
+        (matched["Score"] >= 60) & (matched["Score"] < 95)
     ]
     st.title("")
     if most_probable_match.empty == False:
@@ -137,24 +120,85 @@ if button_clicked:
 
 # function for uploaded supplier list
 # @st.cache
-def entity_matching_for_streamlit(supplier_list, uk_sanction_list):
+# 25th march updated code for upload function
+def entity_matching_for_upload(supplier_list, uk_sanction_data):
+    def join_with_space(x): 
+        return("$".join(x))
+    
     supplier_list = supplier_list["Display Name"].tolist()
-    uk_sanction = uk_sanction_list["Name 6"].tolist()
+    uk_sanction = list(zip(uk_sanction_data["Name 6"].tolist(),uk_sanction_data["Unique ID"].tolist()))
     cross_prod = list(itertools.product(supplier_list, uk_sanction))
     t_dict = {}
+    
     for i in cross_prod:
-        t_dict[i] = f_name_match_score(i[0], i[1])
+        t_dict[i] = int(f_name_match_score(i[0], i[1][0]))
+    print("Score calculation time")
     lst = list(t_dict.items())
     df1 = pd.DataFrame(lst, columns=["Supp Name", "Score"])
-    df1[["Supplier Name", "Sanctioned Name"]] = pd.DataFrame(
-        df1["Supp Name"].tolist(), index=df1.index
+    df1[["Supplier Name", "Sanctioned and Country Code"]] = pd.DataFrame(
+        df1["Supp Name"].tolist(), 
+        index=df1.index
     )
-    df1["Match Found"] = np.where(df1["Score"] > 95, "Yes", "No")
-    df1["Sanctioned Org Name"] = np.where(df1["Score"] > 95, df1["Sanctioned Name"], "")
-    df1.drop(df1.columns[[0, 1, 3]], axis=1, inplace=True)
-    df1.drop_duplicates(keep="first", inplace=True)
-    df1 = df1.set_index("Supplier Name")
-    return df1.to_csv()
+    df1[["Sanctioned Name", "Country Code"]] = pd.DataFrame(
+        df1["Sanctioned and Country Code"].tolist(), 
+        index=df1.index
+    )
+    df1.drop(df1.columns[[0,3]],axis=1,inplace=True)
+    #df1.to_csv('test100.csv')
+    df1 = df1.sort_values(['Supplier Name','Score'],ascending=False).groupby('Supplier Name').head(5)
+    
+    
+    df1['All data'] = np.where(df1['Score']>95,
+                               df1['Sanctioned Name'] + "!" + df1['Country Code'] + "!" + df1['Score'].astype(str),
+                               "! !")
+    
+    df1 = df1.groupby("Supplier Name")["All data"].agg(join_with_space)
+    df1 = df1.reset_index()
+    
+    final_res = df1.join(df1['All data'].str.split('$', expand=True).rename(
+            columns={0:'A', 1:'B', 2:'C', 3:'D', 4:'E'}
+        )
+    )
+    
+    final_res.drop("All data", axis=1, inplace=True)
+    
+    # Match1
+    final_res = final_res.join(final_res['A'].str.split('!', expand=True).rename(
+            columns={0:'Sanctioned Name 1', 1:'Country Code 1', 2:'Score 1'}
+        )
+    )
+    final_res.drop("A", axis=1, inplace=True)
+    
+    # Match2
+    final_res = final_res.join(final_res['B'].str.split('!', expand=True).rename(
+            columns={0:'Sanctioned Name 2', 1:'Country Code 2', 2:'Score 2'}
+        )
+    )
+    final_res.drop("B", axis=1, inplace=True)
+    
+    # Match3
+    final_res = final_res.join(final_res['C'].str.split('!', expand=True).rename(
+            columns={0:'Sanctioned Name 3', 1:'Country Code 3', 2:'Score 3'}
+        )
+    )
+    final_res.drop("C", axis=1, inplace=True)
+    
+    # Match4
+    final_res = final_res.join(final_res['D'].str.split('!', expand=True).rename(
+            columns={0:'Sanctioned Name 4', 1:'Country Code 4', 2:'Score 4'}
+        )
+    )
+    final_res.drop("D", axis=1, inplace=True)
+    
+    # Match5
+    final_res = final_res.join(final_res['E'].str.split('!', expand=True).rename(
+            columns={0:'Sanctioned Name 5', 1:'Country Code 5', 2:'Score 5'}
+        )
+    )
+    final_res.drop("E", axis=1, inplace=True)
+    final_res = final_res.set_index("Supplier Name")
+    
+    return final_res.to_csv()
 
 
 # Upload a supplier list
@@ -166,6 +210,6 @@ if uploaded_file is not None:
     st.write("File uploaded")
     dataframe = pd.read_csv(uploaded_file)
     st.write("Wait for the result")
-    out_file = entity_matching_for_streamlit(dataframe, uk_sanction_list)
+    out_file = entity_matching_for_upload(dataframe, uk_sanction_list)
     st.download_button("Download CSV", out_file, "sanctioned_list.csv", "text/csv")
     st.write("Complete")
--- utils.py
+++ utils.py
@@ -2,7 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import re
+import streamlit as st
+from pandas_ods_reader import read_ods
+from config import sanction_file_name
 
+
+# load uk sanction list
+@st.cache
+def uk_sanction_data_load(sanctions_file):
+    scrape_uk_sanctions()
+    df2 = read_ods(sanctions_file)
+    df2 = df2.iloc[1:]
+    df2.columns = df2.iloc[0]
+    df2 = df2.drop(df2.index[0])
+    #uk_sanctions = df2.loc[df2['Unique ID'].str.startswith('RUS')]
+    uk_sanctions = df2.loc[df2['Individual, Entity, Ship']=='Entity']
+    return uk_sanctions
+
+
+@st.cache
 def scrape_uk_sanctions():
     url = url_scrap()
     filename = "UK_Sanctions_List.ods"
@@ -14,6 +32,8 @@ def scrape_uk_sanctions():
     f.close()
     return
 
+
+@st.cache
 def url_scrap():
     req = Request("https://www.gov.uk/government/publications/the-uk-sanctions-list")
     html_page = urlopen(req)
@@ -25,3 +45,6 @@ def url_scrap():
     r = re.compile(".*.ods")
     nw = list(filter(r.match,ls))
     return nw[0]
+
+# load uk sanctioned data
+uk_sanction_list = uk_sanction_data_load(sanction_file_name)
